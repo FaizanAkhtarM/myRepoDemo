0668071fa26ea21154b23f9e8f9e678a77b54edc
FaizanAkhtarM <faizanakhtar145@gmail.com>
2022-12-28 22:58:24 +0530

    Trails

---
+++ src/org/demo/Dummy.java
@@ -0,0 +1,14 @@
+package org.demo;
+
+public class Dummy {
+	public static void main(String[] args) {
+		int num[] = new int [3];
+		num[0] =  50;
+		num[1] = 100;
+		num[2] = 200;
+		for(int x : num) {
+			System.out.println(x);
+		}
+		
+	}
+}
